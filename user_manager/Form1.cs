16f71be449c970c597d927ff4530a09ebd8ed599
sonicxlg <lucanicosia516@gmail.com>
2023-05-09 19:11:52 +0200

    update 09.05

--- user_manager/Form1.cs
+++ user_manager/Form1.cs
@@ -1,3 +1,6 @@
+using System.Xml.Serialization;
+using System.Diagnostics;
+
 namespace user_manager
 {
     public partial class Form1 : Form
@@ -5,6 +8,16 @@ namespace user_manager
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ciao");
         }
     }
 }

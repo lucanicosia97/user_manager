31fd61474ddfebcaedfb58bf09a753245bf0dd82
nicola.leonetti <nicola.leonetti@simplon.com>
2023-05-10 20:21:51 +0200

    first test

--- user_manager/Form1.Designer.cs
+++ user_manager/Form1.Designer.cs
@@ -28,33 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            lblVorname = new Label();
+            txtVorname = new TextBox();
+            lblNachname = new Label();
+            txtNachname = new TextBox();
+            lblJobtitel = new Label();
+            txtJobtitel = new TextBox();
             SuspendLayout();
             // 
-            // button1
+            // lblVorname
             // 
-            button1.Location = new Point(209, 143);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            lblVorname.AutoSize = true;
+            lblVorname.Location = new Point(12, 9);
+            lblVorname.Name = "lblVorname";
+            lblVorname.Size = new Size(54, 15);
+            lblVorname.TabIndex = 0;
+            lblVorname.Text = "Vorname";
+            lblVorname.Click += label1_Click;
+            // 
+            // txtVorname
+            // 
+            txtVorname.AccessibleName = "txtnachname";
+            txtVorname.Location = new Point(12, 27);
+            txtVorname.Name = "txtVorname";
+            txtVorname.Size = new Size(192, 23);
+            txtVorname.TabIndex = 1;
+            txtVorname.TextChanged += textBox1_TextChanged;
+            // 
+            // lblNachname
+            // 
+            lblNachname.AutoSize = true;
+            lblNachname.Location = new Point(12, 64);
+            lblNachname.Name = "lblNachname";
+            lblNachname.Size = new Size(65, 15);
+            lblNachname.TabIndex = 2;
+            lblNachname.Text = "Nachname";
+            lblNachname.Click += label2_Click;
+            // 
+            // txtNachname
+            // 
+            txtNachname.Location = new Point(12, 82);
+            txtNachname.Name = "txtNachname";
+            txtNachname.Size = new Size(192, 23);
+            txtNachname.TabIndex = 3;
+            // 
+            // lblJobtitel
+            // 
+            lblJobtitel.AccessibleName = "labeljobtitel";
+            lblJobtitel.AutoSize = true;
+            lblJobtitel.Location = new Point(12, 119);
+            lblJobtitel.Name = "lblJobtitel";
+            lblJobtitel.Size = new Size(45, 15);
+            lblJobtitel.TabIndex = 4;
+            lblJobtitel.Text = "Jobtitel";
+            // 
+            // txtJobtitel
+            // 
+            txtJobtitel.AccessibleName = "txtjobtitel";
+            txtJobtitel.Location = new Point(12, 137);
+            txtJobtitel.Name = "txtJobtitel";
+            txtJobtitel.Size = new Size(192, 23);
+            txtJobtitel.TabIndex = 5;
+            txtJobtitel.Tag = "";
+            txtJobtitel.TextChanged += txtjobtitel_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(txtJobtitel);
+            Controls.Add(lblJobtitel);
+            Controls.Add(txtNachname);
+            Controls.Add(lblNachname);
+            Controls.Add(txtVorname);
+            Controls.Add(lblVorname);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Label lblVorname;
+        private TextBox txtVorname;
+        private Label lblNachname;
+        private TextBox txtNachname;
+        private Label lblJobtitel;
+        private TextBox txtJobtitel;
     }
 }
--- user_manager/Form1.cs
+++ user_manager/Form1.cs
@@ -19,5 +19,25 @@ namespace user_manager
         {
             MessageBox.Show("Hallo");
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtjobtitel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
